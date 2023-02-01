bea509f7971437cc498ea48aa4193aa69e23f402
Cert Ready <120624638+Cert-Ready@users.noreply.github.com>
2023-02-01 14:28:38 -0600

    Change canvas color on Hover

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ const canvasEl = document.querySelector('.canvas');
 const sliderEl = document.querySelector('#grid-slider');
 const gridSizeEl = document.querySelectorAll('.grid-size');
 const squareCount = document.querySelector('.square-count');
+const clearBtnEl = document.querySelector('.clear-btn');
 
 // slider value
 const sliderElValue = sliderEl.value;
@@ -14,15 +15,34 @@ canvasEl.style.cssText = `
     grid-template-rows: repeat(${sliderElValue}, 1fr);
 `;
 
+const defaultBackground = 'rgb(245,245,245)';
+const classicBg = 'rgb(169,169,169)';
+
+const classicColor = (event) => {
+  return (event.target.style.backgroundColor = `${classicBg}`);
+};
+
 // create grid
 for (let i = 0; i < gridSize; i++) {
   let canvasSquare = document.createElement('div');
   canvasSquare.style.cssText = `
-  background-color:  rgb(245,245,245);
+  background-color:  ${defaultBackground};
   outline: 1px solid var(--dark-0); 
   `;
   canvasEl.appendChild(canvasSquare);
 }
+
+clearBtnEl.addEventListener('click', () => clearCanvas(canvasEl.childNodes));
+canvasEl.addEventListener('mouseover', classicColor);
+
+// clear canvas
+const clearCanvas = (element) => {
+  // console.log(element);
+  element.forEach((node) => {
+    // console.log(node);
+    node.style.backgroundColor = defaultBackground;
+  });
+};
 
 // QOL display grid size on DOM
 gridSizeEl.forEach((element) => {
@@ -30,7 +50,3 @@ gridSizeEl.forEach((element) => {
 });
 
 squareCount.textContent = gridSize;
-
-console.log('slider value = ' + sliderElValue);
-console.log(typeof gridSize);
-console.log(gridSize);
